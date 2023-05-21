ba61f70702ab16419b77bb742151ef7e77ce2f7a
0sugo <josugo26@gmail.com>
2023-05-21 23:32:44 +0300

    Add pumpkin image on nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,9 @@
 import { NavLink } from "react-router-dom";
-
+import logo from '../images/pumpkin.png';
 const Navbar = () => {
   return (
     <div className='Navbar'>
+        <img src={logo} />
         <NavLink to="/">Stocks</NavLink>
         <NavLink to="/Order">Order</NavLink>
     </div>
