962afb9becde6db38f2f5b6d112c078752e952a6
0sugo <josugo26@gmail.com>
2023-05-27 01:19:09 +0300

    Add routes for added components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import Stock from "./components/Stock";
 import Order from "./components/Order";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Butchery from "./components/Butchery";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <Routes>
-        <Route path="/" element={<Stock />}/>
+        <Route path="/" element={<Home />}/>
+        <Route path="Stock" element={<Stock />}/>
         <Route path="Order" element={<Order />}/>
+        <Route path="Butchery" element={<Butchery/>}/>
       </Routes>
 
     </div>
