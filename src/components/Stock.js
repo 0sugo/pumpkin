2c09c76af168f3fad4c67fe91496bc869eeb474b
0sugo <josugo26@gmail.com>
2023-05-31 23:54:23 +0300

    Add products to stocks page

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,10 +1,11 @@
 import Navbar from "./Navbar"
-
+import Products from "./Products"
 const Stock = () => {
   return (
     <div>
      <Navbar />
      <p>Stocks area is under construction</p>
+     <Products />
     </div>
   )
 }
