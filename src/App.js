d425a93e9598f5fb993ed1a3b88c32015795301f
Joseck Osugo <josugo26@gmail.com>
2023-09-15 23:57:37 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Stock from './components/Stock';
 import Order from './components/Order';
 import Home from './components/Home';
 import Butchery from './components/Butchery';
-qwertyu
+qwerty
 function App() {
   return (
     <div className="App">
