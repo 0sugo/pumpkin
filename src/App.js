467fd70b63b42af17514a924c7da591419586482
0sugo <josugo26@gmail.com>
2023-09-27 23:57:14 +0300

    Add next UI

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
+import { NextUIProvider } from '@nextui-org/react';
+import '@fontsource/poppins';
 import Stock from './components/Stock';
 import Order from './components/Order';
 import Home from './components/Home';
@@ -6,14 +8,15 @@ import Butchery from './components/Butchery';
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="Stock" element={<Stock />} />
-        <Route path="Order" element={<Order />} />
-        <Route path="Butchery" element={<Butchery />} />
-      </Routes>
-
+    <div className="App bg-[#1B1F2C]">
+      <NextUIProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="Stock" element={<Stock />} />
+          <Route path="Order" element={<Order />} />
+          <Route path="Butchery" element={<Butchery />} />
+        </Routes>
+      </NextUIProvider>
     </div>
   );
 }
