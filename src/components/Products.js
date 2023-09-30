28dd4a443584762e7c037744ece78f308efc0c54
0sugo <josugo26@gmail.com>
2023-09-30 23:53:10 +0300

    Add remove functionality on icon click

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,17 +1,17 @@
 /* eslint-disable */
-import React, { useEffect, useRef, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import  beer from '../images/beer-outline.svg';
-import pint from '../images/pint-outline.svg';
-import wine from '../images/wine-outline.svg';
-import restaurant from '../images/restaurant-outline.svg';
-import print from '../images/print-outline.svg';
-import cart from '../images/cart-outline.svg';
-import add from '../images/add-circle-outline.svg';
-import subtract from '../images/remove-circle-outline.svg';
-import close from '../images/close-circle-outline.svg';
+import React, { useEffect, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import beer from "../images/beer-outline.svg";
+import pint from "../images/pint-outline.svg";
+import wine from "../images/wine-outline.svg";
+import restaurant from "../images/restaurant-outline.svg";
+import print from "../images/print-outline.svg";
+import cart from "../images/cart-outline.svg";
+import add from "../images/add-circle-outline.svg";
+import subtract from "../images/remove-circle-outline.svg";
+import close from "../images/close-circle-outline.svg";
 
-import { fetchAllProducts } from '../redux/Products/productsSlice';
+import { fetchAllProducts } from "../redux/Products/productsSlice";
 
 const Products = () => {
   const products = useSelector((store) => store.allProducts);
@@ -21,9 +21,9 @@ const Products = () => {
   const [isPrinting, setIsPrinting] = useState(false);
   const dispatch = useDispatch();
 
-  useEffect(()=>{
+  useEffect(() => {
     dispatch(fetchAllProducts());
-  },[dispatch]);
+  }, [dispatch]);
 
   const updateQuantity = (name, quantity) => {
     const updatedItems = selectedItems.map((item) => {
@@ -38,20 +38,24 @@ const Products = () => {
 
   const handleItemClick = (item) => {
     if (!enterPressed) {
-      const existingItem = selectedItems.find((selectedItem) => selectedItem.name === item.name);
+      const existingItem = selectedItems.find(
+        (selectedItem) => selectedItem.name === item.name
+      );
 
       if (existingItem) {
         updateQuantity(item.name, existingItem.quantity + 1);
       } else {
-        setSelectedItems([...selectedItems, { name: item.name, price: item.price, quantity: 1 }]);
+        setSelectedItems([
+          ...selectedItems,
+          { name: item.name, price: item.price, quantity: 1 },
+        ]);
       }
     }
     setEnterPressed(false);
   };
 
-
   const handleItemKeyPress = (item, event) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       setEnterPressed(true);
       handleItemClick(item);
     }
@@ -124,14 +128,18 @@ const Products = () => {
             </tr>
           </thead>
           <tbody>
-            ${selectedItems.map((item) => `
+            ${selectedItems
+        .map(
+          (item) => `
               <tr>
                 <td>${item}</td>
                 <td>1</td>
                 <td>Ksh 10</td>
                 <td>Ksh 10</td>
               </tr>
-            `).join('')}
+            `
+        )
+        .join("")}
           </tbody>
         </table>
         <p>V.A.T: 16%</p>
@@ -148,17 +156,15 @@ const Products = () => {
     </html>
   `;
 
-// Function to calculate the total amount based on selected items
-function calculateTotal(selectedItems) {
-  // You can calculate the total amount based on the selected items here
-  // Replace this with your calculation logic
-  return "100"; // Example total amount
-}
-
-
+    // Function to calculate the total amount based on selected items
+    function calculateTotal(selectedItems) {
+      // You can calculate the total amount based on the selected items here
+      // Replace this with your calculation logic
+      return "100"; // Example total amount
+    }
 
     // Open a new print window or create an iframe
-    const printWindow = window.open('', '', 'width=600,height=400');
+    const printWindow = window.open("", "", "width=600,height=400");
     printWindow.document.open();
     // printWindow.document.write(`<html><head><title>Receipt</title></head><body><ul>${printContents}</ul></body></html>`);
     printWindow.document.write(printContents);
@@ -181,100 +187,142 @@ function calculateTotal(selectedItems) {
     return total;
   }
 
+  const removeItem = (name) => {
+    const updatedItems = selectedItems.filter((item) => item.name !== name);
+    setSelectedItems(updatedItems);
+  };
+
   return (
     <div className="products-container flex flex-row px-4 gap-4 text-white relative">
-      <div className="receipt-area hide-scrollbar rounded-2xl basis-1/2 bg-[#252A3C] p-4 max-h-screen overflow-y-scroll overflow-x-hidden bg-scroll" id="receipt-area" ref={receiptRef}>
-        <p className='text-center'>RECEIPT AREA</p>
+      <div
+        className="receipt-area hide-scrollbar rounded-2xl basis-1/2 bg-[#252A3C] p-4 max-h-screen overflow-y-scroll overflow-x-hidden bg-scroll"
+        id="receipt-area"
+        ref={receiptRef}
+      >
+        <p className="text-center">RECEIPT AREA</p>
+
         <table className="w-full table-fixed">
-  <thead>
-    <tr>
-      <th className="text-start py-4">Item</th>
-      <th className="text-center">Qty</th>
-      <th className="text-center">Price</th>
-      <th className="text-center">Amount</th>
-    </tr>
-  </thead>
-  <tbody>
-  {selectedItems.map((item, index) => (
-    <tr key={index} className={index % 2 === 0 ? 'bg-[#1B1F2C]' : 'bg-inherit'}>
-      <td className="w-2/5 text-start px-2">{item.name}</td>
-      <td className="flex grow-0 justify-between items-center">
-        <button
-          onClick={() => updateQuantity(item.name, item.quantity - 1)}
-          disabled={item.quantity <= 1}
-        ><span className=''><img img src={subtract} alt='subtract' width="25px" height="25px"/></span>
-        </button>
-        <span className="m-5">{item.quantity}</span>
-        <button onClick={() => updateQuantity(item.name, item.quantity + 1)}
-        ><span><img img src={add} alt='adds' width="25px" height="25px"/></span></button>
-      </td>
-      <td className=" w-1/5 text-center">{item.price}</td>
-      <td className="w-1/5 text-center">Ksh {item.price * item.quantity}</td>
-    </tr>
-  ))}
-</tbody>
-</table>
+          <thead>
+            <tr>
+              <th className="w-2/5 py-4 text-start">Item</th>
+              <th className="w-1/5 text-center">Qty</th>
+              <th className="w-1/5 text-center">Price</th>
+              <th className="w-1/5 text-center">Amount</th>
+            </tr>
+          </thead>
+          <tbody>
+            {selectedItems.map((item, index) => (
+              <tr
+                key={index}
+                className={index % 2 === 0 ? "bg-[#1B1F2C]" : "bg-inherit"}
+              >
+                <td className="px-2">
+                  <div className="flex items-center">
+                    <span className="w-8 h-8 cursor-pointer" onClick={()=>{removeItem(item.name)}} >
+                      <img
+                        src={close}
+                        alt="remover"
+                        width="25px"
+                        height="25px"
+                      />
+                    </span>
+                    <span className="flex-grow">{item.name}</span>
+                  </div>
+                </td>
+                <td className="flex justify-between items-center">
+                  <button
+                    onClick={() => updateQuantity(item.name, item.quantity - 1)}
+                    disabled={item.quantity <= 1}
+                    className="w-8 h-8"
+                  >
+                    <img
+                      src={subtract}
+                      alt="subtract"
+                      width="25px"
+                      height="25px"
+                    />
+                  </button>
+                  <span className="m-4">{item.quantity}</span>
+                  <button
+                    onClick={() => updateQuantity(item.name, item.quantity + 1)}
+                    className="w-8 h-8"
+                  >
+                    <img src={add} alt="add" width="25px" height="25px" />
+                  </button>
+                </td>
+                <td className="w-1/5 text-center">{item.price}</td>
+                <td className="w-1/5 text-center">
+                  Ksh {item.price * item.quantity}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
 
-    <hr className='bg-white w-full'/>
-  <div className="flex justify-center ">
-    <h3>Total :
-    <span> {calculateTotal(selectedItems)}</span></h3>
-  </div>
+        <hr className="bg-white w-full" />
+        <div className="flex justify-center ">
+          <h3>
+            Total :<span> {calculateTotal(selectedItems)}</span>
+          </h3>
+        </div>
 
-
-        <div className='flex'>
-        <button
-          type="button"
-          className="bg-[#EB5757] p-4 m-10 rounded-lg mx-auto sticky flex gap-1"
-          onClick={openPrintWindow}
-          disabled={isPrinting || selectedItems.length === 0}
-        >
-          PRINT RECEIPT
-          <span><img src={print} className='h-6'/></span>
-        </button>
+        <div className="flex">
+          <button
+            type="button"
+            className="bg-[#EB5757] p-4 m-10 rounded-lg mx-auto sticky flex gap-1"
+            onClick={openPrintWindow}
+            disabled={isPrinting || selectedItems.length === 0}
+          >
+            PRINT RECEIPT
+            <span>
+              <img src={print} className="h-6" />
+            </span>
+          </button>
         </div>
       </div>
-        <div className='list-all-products '>
-          <div className='flex justify-center gap-4 items-center overflow-hidden'>
-            <div className='flex flex-col items-center gap-1'>
-              <img src={beer} alt='Beers' width="100px" height="100px" />
-              <p>Beers</p>
-            </div>
-
-            <div className='flex flex-col items-center'>
-            <img src={pint} alt='pint' width="100px" height="100px" />
-            <p>Liqour</p>
-            </div>
-
-            <div className='flex flex-col items-center'>
-              <img src={wine} alt='Wines' width="100px" height="100px" />
-              <p>Wine</p>
-
-            </div>
-
-            <div className='flex flex-col items-center'>
-              <img src={restaurant} alt='Restaurant' width="100px" height="100px" />
-              <p>Restaurant</p>
-
-            </div>
-
+      <div className="list-all-products ">
+        <div className="flex justify-center gap-4 items-center overflow-hidden">
+          <div className="flex flex-col items-center gap-1">
+            <img src={beer} alt="Beers" width="100px" height="100px" />
+            <p>Beers</p>
           </div>
 
-          <ul className="grid grid-cols-3 xl:grid-cols-4 xl:basis-3/4 basis-1/2 gap-4 p-3 bg-[#1B1F2C] static">
-            {products.products.map((item) => (
-              <button
-                key={item.id}
-                className=" flex flex-wrap items-center justify-around rounded-lg box-border lg:h-16 md:h-20 uppercase bg-[#252A3C] font-semibold"
-                type="button"
-                onClick={() => handleItemClick(item)}
-                onKeyPress={(e) => handleItemKeyPress(item, e)}
-                tabIndex={0}
-              >
-                {item.name}
-              </button>
-            ))}
-          </ul>
+          <div className="flex flex-col items-center">
+            <img src={pint} alt="pint" width="100px" height="100px" />
+            <p>Liqour</p>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <img src={wine} alt="Wines" width="100px" height="100px" />
+            <p>Wine</p>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <img
+              src={restaurant}
+              alt="Restaurant"
+              width="100px"
+              height="100px"
+            />
+            <p>Restaurant</p>
+          </div>
         </div>
+
+        <ul className="grid grid-cols-3 xl:grid-cols-4 xl:basis-3/4 basis-1/2 gap-4 p-3 bg-[#1B1F2C] static">
+          {products.products.map((item) => (
+            <button
+              key={item.id}
+              className=" flex flex-wrap items-center justify-around rounded-lg box-border lg:h-16 md:h-20 uppercase bg-[#252A3C] font-semibold"
+              type="button"
+              onClick={() => handleItemClick(item)}
+              onKeyPress={(e) => handleItemKeyPress(item, e)}
+              tabIndex={0}
+            >
+              {item.name}
+            </button>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
