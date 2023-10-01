cde3f7ef3de8bae1deb8bcc0174e8b5bc8c5e968
0sugo <josugo26@gmail.com>
2023-10-01 23:04:27 +0300

    Update navbar to show unlisted payments

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,17 @@ import { NavLink } from 'react-router-dom';
 import logo from '../images/pumpkin.svg';
 import logout from '../images/log-out-outline.svg';
 import home from '../images/home-outline.svg';
+import linker from '../images/link-outline.svg';
 
 const Navbar = () => (
   <div className="main-nav flex items-center justify-between mx-32 text-[#EB5757]">
     <img src={logo} alt="logo" className="logo bg-inherit" width="80px" height="80px" />
     <div className="mini-navbar text-xl flex">
+      <NavLink to="/Stock" className="mx-4 flex items-center gap-1">
+        Outstanding
+        <span><img src={linker} alt="link payment" className="h-6" /></span>
+      </NavLink>
+
       <NavLink to="/" className="mx-4 flex items-center gap-1">
         Home
         <span><img src={home} alt="home" className="h-6" /></span>
@@ -15,7 +21,6 @@ const Navbar = () => (
         Logout
         <span><img src={logout} alt="logout" className="h-6" /></span>
       </NavLink>
-      {/* <NavLink to="/Stock">Stock</NavLink> */}
       {/* <NavLink to="/Order">Order</NavLink> */}
     </div>
   </div>
