8cd502a89159d1cff6b7680d55e5165b163e3e54
0sugo <josugo26@gmail.com>
2023-10-03 17:45:50 +0300

    Update navbar in home

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 const Home = () => (
   <div className="home-links">
     <NavLink to="/Order" className="links">ORDER DRINKS</NavLink>
-    <NavLink to="/Stock" className="links">STOCK-TAKE</NavLink>
+    <NavLink to="/Outstanding" className="links">Outstanding</NavLink>
     <NavLink to="/Butchery" className="links">BUTCHERY</NavLink>
   </div>
 );
