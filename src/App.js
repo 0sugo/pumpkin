2bb26f5a0ecd152fa43cce23e7f1440c789ada60
0sugo <josugo26@gmail.com>
2023-10-03 14:32:05 +0300

    Update app with relvant components only

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import { NextUIProvider } from '@nextui-org/react';
 import '@fontsource/poppins';
-import Stock from './components/Stock';
+import Outstanding from './components/Outstanding';
 import Order from './components/Order';
 import Home from './components/Home';
 import Butchery from './components/Butchery';
@@ -12,7 +12,7 @@ function App() {
       <NextUIProvider>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="Stock" element={<Stock />} />
+          <Route path="Outstanding" element={<Outstanding />} />
           <Route path="Order" element={<Order />} />
           <Route path="Butchery" element={<Butchery />} />
         </Routes>
