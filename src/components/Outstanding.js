a48488e2bb02ca96e457b92310b170d99efaa547
0sugo <josugo26@gmail.com>
2023-10-03 14:33:00 +0300

    Create outstanding component

---
+++ src/components/Outstanding.js
@@ -0,0 +1,10 @@
+import Navbar from './Navbar';
+
+const Outstanding = () => (
+  <div>
+    <Navbar />
+    <h3 className="text-2xl font-bold underline">Outstanding area is under construction</h3>
+  </div>
+);
+
+export default Outstanding;
