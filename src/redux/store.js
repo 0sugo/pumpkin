571ba5874823f19e4a76bd3bb97ccdbc98c7a7ad
0sugo <josugo26@gmail.com>
2023-10-03 17:45:18 +0300

    Add fetched customers to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productsSliceReducer from './Products/productsSlice';
+import customersSliceReducer from './customers/customersSlice';
 
 const store = configureStore({
   reducer: {
     allProducts: productsSliceReducer,
-
+    allCustomers: customersSliceReducer,
   },
 });
 
