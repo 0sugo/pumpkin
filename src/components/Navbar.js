b8c64cec0aac8c32b25dd4a833ebb938dc2f80b8
0sugo <josugo26@gmail.com>
2023-10-03 14:32:41 +0300

    Fix nav bar bug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,16 +3,15 @@ import logo from '../images/pumpkin.svg';
 import logout from '../images/log-out-outline.svg';
 import home from '../images/home-outline.svg';
 import linker from '../images/link-outline.svg';
-q
+
 const Navbar = () => (
   <div className="main-nav flex items-center justify-between mx-32 text-[#EB5757]">
     <img src={logo} alt="logo" className="logo bg-inherit" width="80px" height="80px" />
     <div className="mini-navbar text-xl flex">
-      <NavLink to="/Stock" className="mx-4 flex items-center gap-1">
+      <NavLink to="/Outstanding" className="mx-4 flex items-center gap-1">
         Outstanding
         <span><img src={linker} alt="link payment" className="h-6" /></span>
       </NavLink>
-
       <NavLink to="/" className="mx-4 flex items-center gap-1">
         Home
         <span><img src={home} alt="home" className="h-6" /></span>
